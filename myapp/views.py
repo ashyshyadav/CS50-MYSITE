f47c3bb2f953a491bc7d87a2b77a38fd8663b594
ashyshyadav <71547800+ashyshyadav@users.noreply.github.com>
2021-02-17 20:41:54 +0530

    first commit

---
+++ myapp/views.py
@@ -0,0 +1,12 @@
+import datetime
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+def index(request):
+    return HttpResponse('Hello, World!')
+
+
+def greet(request, name):
+    return render(request, "myapp/index.html", {'name': name})
+    
